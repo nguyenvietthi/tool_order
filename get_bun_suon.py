84af4005a0feead46104e4266cfc5c83c545d354
nguyenvietthi <nguyenvietthi.nd@gmail.com>
2023-04-24 13:32:32 +0700

    update get_bun_suon.py

--- get_bun_suon.py
+++ get_bun_suon.py
@@ -4,7 +4,7 @@ import re
 
 list_user = { "live:.cid.a821106b3f180f97": "Quangnd1", "live:.cid.6f77ed61558db69": "V.anh", "live:.cid.eccff1ee01674be4": "Hoàn", "live:.cid.154c2e3316abbe42": "Quangnd0", "live:.cid.fcd17658776516e8": "Đạt", "live:minhdz2612": "Minh", "live:maytinhhuugiai": "Huy", "live:.cid.7b4731d55e5dea1c": "Thịnh", "live:bff4e847288cf939": "Lê Văn Hưng", "live:.cid.b594fc7b5970aa0d": "Phạm Quang Anh", "live:.cid.200aa59940c71a73": "Huy Trần", "live:b7c3ac2e7e329d51": "A.Huy", "live:.cid.fe2091fd5a44b92": "Thi", "manhnd.dti": "A.mạnh", "live:.cid.5498c54b35e01186": "V.Huy", "live:.cid.37617356a8747fc4": "HuyBu", "live:.cid.b81a24a561b43f1a": "Phương", "live:.cid.7c2f78584abceb6a": "Duy", "live:.cid.e6d8d6c10f7c6b3f": "Hiền", "live:lam.pn_1": "Lâm"  }
 
-sk = Skype("0366939260", "")
+sk = Skype("0366939260", "vietthind1147258")
 
 chat = sk.chats
 contacts = sk.contacts
@@ -39,10 +39,10 @@ for order_user in list_order_user:
     content_remove_plus = content.replace("+", "")
     index_trash = content.find("<")
     # print(index_trash)
-    if(index_trash != -1):
-        content_remove = content[:index_trash]
-    else:
-        content_remove = content
+    # if(index_trash != -1):
+    content_remove = content[:index_trash]
+    # else:
+    #     content_remove = content
     content_remove = content_remove.replace("\n", "")
     number_of_order = sum([int(s) for s in content_remove_plus.split() if s.isdigit()])
     total = total + number_of_order
